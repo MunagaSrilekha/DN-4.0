41f3efea7fe1f06fca3e6172711f0fb19d90a042
MunagaSrilekha <srilekha7038@gmail.com>
2025-07-13 15:31:16 +0530

    Commited the code changes

--- src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,26 +1,11 @@
 package com.cognizant.springlearn;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+@SpringBootApplication
 public class SpringLearnApplication {
-
     public static void main(String[] args) {
-        displayDate();
-    }
-
-    public static void displayDate() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
-
-        try {
-            Date date = format.parse("31/12/2018");
-            System.out.println("Parsed Date: " + date);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SpringApplication.run(SpringLearnApplication.class, args);
     }
 }
