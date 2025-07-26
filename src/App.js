2c140ed7dde4b8a9094fab568b0beb5dac4ee2ce
MunagaSrilekha <srilekha7038@gmail.com>
2025-07-26 22:23:13 +0530

    Initialized-commit Cohortdashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CohortDetails from './Components/CohortDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My Academy Dashboard</h2>
+      <CohortDetails name="React Bootcamp" trainer="John Doe" status="ongoing" />
+      <CohortDetails name="Java Spring" trainer="Jane Smith" status="completed" />
     </div>
   );
 }
