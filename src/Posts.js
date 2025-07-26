43f99852b031d2370b73fac23ad118b72f599d63
MunagaSrilekha <srilekha7038@gmail.com>
2025-07-26 22:10:05 +0530

    Initialized commit-blogapp

---
+++ src/Posts.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => this.setState({ posts: data }))
+      .catch(error => {
+        console.error('Error loading posts:', error);
+        this.setState({ hasError: true });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert('An error occurred while loading the posts.');
+    console.error('Error caught by componentDidCatch:', error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong. Please try again later.</h2>;
+    }
+
+    return (
+      <div>
+        <h2>Blog Posts</h2>
+        {this.state.posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
